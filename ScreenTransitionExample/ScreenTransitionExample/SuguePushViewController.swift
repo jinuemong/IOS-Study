92de8baad75228457f6e6c1e3a40ae687cf694af
김진우 <jinwoo3661@naver.com>
2023-05-04 18:05:21 +0900

    ScreenTransition 프로젝트 실습

---
+++ ScreenTransitionExample/ScreenTransitionExample/SuguePushViewController.swift
@@ -0,0 +1,9 @@
+
+
+import UIKit
+
+class SuguePushViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ScreenTransitionExample/ScreenTransitionExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenTransitionExample
+//
+//  Created by 김진우 on 2023/05/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
